e746ebb54b169cc0a618f2fda3d8a7f0a56e0d8c
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-05 14:25:51 -0500

    Educator enum

---
+++ src/main/java/io/zipcoder/interfaces/Educator.java
@@ -0,0 +1,36 @@
+package io.zipcoder.interfaces;
+
+public enum Educator implements Teacher {
+    LEON(Instructors.getInstance().findById(1L)),
+    DOLIO(Instructors.getInstance().findById(2L)),
+    KRIS(Instructors.getInstance().findById(3L));
+
+    private Instructor instructor;
+
+    private Double timeWorked = 0.0;
+
+    Educator(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        this.instructor.teach(learner, numberOfHours);
+        setTimeWorked(numberOfHours);
+
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+        this.instructor.lecture(learners, numberOfHours);
+        setTimeWorked(numberOfHours);
+    }
+
+    public void setTimeWorked(Double timeWorked) {
+        this.timeWorked += timeWorked;
+    }
+
+    public Double getTimeWorked() {
+        return this.timeWorked;
+    }
+}
